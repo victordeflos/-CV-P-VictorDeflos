0afc415221465cd2147e570ad0c5be1bc6ded116
Viktor Blazhevski <victordeflos@hotmail.com>
2021-04-20 11:39:39 +0200

    Initial setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
+import App from "./App";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
